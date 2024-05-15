0ebc9a82f8a3f3751f8914da765764dec78fd59e
Samar-Memon <samar56786raza@gmail.com>
2024-05-15 17:15:11 +0500

    New-Calculator

--- app.js
+++ app.js
@@ -11,11 +11,22 @@ const emptyScreen = () => {
     document.getElementById('screen').value = displayValue;
 }
 
+document.querySelector('.history-icon').addEventListener('click', () => {
+    document.querySelector('.history-group').classList.toggle('top');
+});
+let timeSpan = document.querySelector('.time');
+
+let todayTime = new Date();
+let timeLocal = todayTime.toLocaleTimeString();
 
 const calc = () => {
     let res;
     try{
         res = Function('"use strict"; return (' + displayValue + ')')();
+        let createLi = document.createElement('li');
+        createLi.innerHTML = `<span class="time">${timeLocal}</span>
+                                            <span class="calculate">${document.getElementById('screen').value}=</span>`;
+        document.querySelector('.history-group').appendChild(createLi);
         document.getElementById('top').innerHTML = `${document.getElementById('screen').value}=`;
         document.getElementById('screen').value = res;
         displayValue = res.toString();
@@ -33,12 +44,11 @@ document.getElementById('c').addEventListener('click', () => {
 const backSpace = () => {
     let  slice =document.getElementById('screen').value.slice(0,-1);
     document.getElementById("screen").value=slice
-    if(document.getElementById('screen').value == ''){
+    if(document.getElementById('screen').value = ''){
         displayValue = '';
         document.getElementById('screen').value = displayValue;
     }
 }
-
 
 document.querySelector('.fa-expand').addEventListener('click', () => {
     document.querySelector('.calculator').classList.toggle('fullScreen');
